0a4484d8159d2a155f0bce941c54f5454acc95a1
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2023-06-08 14:42:50 +0900

    Feat: back 기본 세팅 및 accounts app 추가

---
+++ backend/accounts/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import User
---
+++ backend/accounts/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+# from django.core.validators import RegexValidator
+from django.db import models
+
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True)
+    hospitalID = models.TextField(blank=True)
+    
+    # 휴대전화 번호
+    # phone_number = models.CharField(
+    #     max_length=13,
+    #     blank=True,
+    #     validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")],
+    # )
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"

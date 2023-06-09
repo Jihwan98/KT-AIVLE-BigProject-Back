1f934a3b1d7dec237ba2fc2a3c515f5eeb09cdd0
jihwan98 <zxwlghksv@gmail.com>
2023-06-09 14:15:59 +0900

    build: accounts app 세팅

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,29 @@ from django.contrib.auth.models import AbstractUser
 # from django.core.validators import RegexValidator
 from django.db import models
 
+class Hospital(models.Model):
+    id = models.IntegerField(primary_key=True)
+    address = models.CharField(max_length=255)
+    officenumber = models.CharField(db_column='officeNumber', max_length=255, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'hospital'
+        
+class Pet(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    species = models.CharField(max_length=255)
+    birth = models.DateTimeField()
+    ownerid = models.ForeignKey("User", on_delete=models.CASCADE, db_column='ownerID')  # Field name made lowercase.
+    gender = models.IntegerField(blank=True, null=True)
+    neutralization = models.IntegerField(blank=True, null=True)
+    adoptiondate = models.DateTimeField(db_column='adoptionDate', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        db_table = 'pet'
 
 class User(AbstractUser):
-    hospitalID = models.TextField(blank=True)
+    hospitalID = models.ForeignKey("Hospital", on_delete=models.CASCADE, blank=True, null=True, db_column="hospitalID")
     
     # 휴대전화 번호
     # phone_number = models.CharField(

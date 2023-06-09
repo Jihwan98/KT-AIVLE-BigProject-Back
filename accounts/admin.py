fbc32b7e7852f92bdcc2f1cd67fbc551fa672aeb
jihwan98 <zxwlghksv@gmail.com>
2023-06-09 16:33:26 +0900

    feat: admin.register 추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import User
+from .models import *
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Hospital)
+class HospitalAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
     pass
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    pass
+@admin.register(History)
+class HistoryAdmin(admin.ModelAdmin):
+    pass

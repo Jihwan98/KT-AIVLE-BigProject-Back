4aa78dc5ccb5fd93fb06e539b06e22b97a917927
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2023-06-12 03:56:39 +0900

    feat: picture api, admin.py 구현

--- posts/models.py
+++ posts/models.py
@@ -26,7 +26,8 @@ class Answer(models.Model):
 
 class Picture(models.Model):
     id = models.IntegerField(primary_key=True)
-    path = models.CharField(max_length=255)
+    # id = models.AutoField(primary_key=True)
+    photo = models.ImageField(upload_to="picture/post/%Y/%m/%d")
 
     class Meta:
         db_table = 'picture'

4b390d4419777b74a1bd3fe70fbe9407e83e3cad
HongkyuRyu <minsubrother@naver.com>
2023-06-12 16:10:52 +0900

    secret.py 생성

---
+++ secret.py
@@ -0,0 +1,21 @@
+# secret 정보 여기다가 모아둠
+SOCIAL_INFO = {
+    "SECRET_KEY":"django-insecure-u^dwaltg9ji$czy^1=z=d(6ue=(-b5pb316l$bhi5j*uxgu95b",
+    "MAIN_DOMAIN":"http://127.0.0.1:8000",
+    "NAVER_CLIENT_ID":"Qd5paRgcxlgMxClKlirF",
+    "NAVER_CLIENT_SECRET":"E98qZUqjxy",
+    "STATE":"NAVER_LOGIN_STRING",
+    "SOCIALACCOUNT_PROVIDERS":{
+        "naver":{
+            "APP":{
+                "client_id":"IRgfKb5GJoBUhJb2xHFm",
+                "secret":"KIjZ6wTLF3",
+                "key":""
+            }
+        }
+        # 카카오
+        # -- 추가 예정 --
+        # 구글
+        # -- 추가 예정 -- 
+    }
+}

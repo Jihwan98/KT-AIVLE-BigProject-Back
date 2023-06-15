72e45e5728288cd31f5d987bbed8c0c7e9e07f8f
HongkyuRyu <minsubrother@naver.com>
2023-06-15 17:01:39 +0900

    oauth

--- backend/urls.py
+++ backend/urls.py
@@ -18,11 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 # from django.conf.urls import include
-
+from allauth.account.views import ConfirmEmailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('posts/',include('posts.urls')),
     
 ]
--- secret.py
+++ secret.py
@@ -4,6 +4,13 @@ SOCIAL_INFO = {
     "MAIN_DOMAIN":"http://127.0.0.1:8000",
     "NAVER_CLIENT_ID":"IRgfKb5GJoBUhJb2xHFm",
     "NAVER_CLIENT_SECRET":"KIjZ6wTLF3",
+    # KAKAO_REST_KEY
+    "KAKAO_REST_API_KEY": "0e1fa375bb3d9b356f7ec7213157ccb0",
+    "KAKAO_REDIRECT_URI":"http://127.0.0.1:8000/accounts/kakao/callback/",
+     # GOOGLE
+    "GOOGLE_CLIENT_ID": "375584534650-k59r41tkahfjikkq2r3qod42bis6851d.apps.googleusercontent.com",
+    "GOOGLE_CLIENT_SECRET": "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",
+     
     "STATE":"NAVER_LOGIN_STRING",
     "SOCIALACCOUNT_PROVIDERS":{
         "naver":{
@@ -12,8 +19,20 @@ SOCIAL_INFO = {
                 "secret":"KIjZ6wTLF3",
                 "key":""
             }
-        }
+        },
         # 카카오
+        "kakao": {
+            
+        },
+        # 구글
+        'google' : {
+            "APP": {
+                "client_id": "375584534650-k59r41tkahfjikkq2r3qod42bis6851d.apps.googleusercontent.com",
+                "secret": "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",
+                "key": ""
+            }
+        },
+        
         # -- 추가 예정 --
         # 구글
         # -- 추가 예정 -- 
